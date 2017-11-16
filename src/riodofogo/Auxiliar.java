aef78a52b7711c20689400f1208c20e9f865c263
João Carias <joaocariasdefranca@gmail.com>
2017-11-16 14:54:20 -0300

    first commit

---
+++ src/riodofogo/Auxiliar.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package riodofogo;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+/**
+ *
+ * @author joao.franca
+ */
+public class Auxiliar {
+    /**
+     * 
+     * @param caminho
+     * @return
+     * @throws FileNotFoundException 
+     */
+    public static BufferedReader leituraArquivo(String caminho) throws FileNotFoundException{
+        FileReader arquivo = new FileReader(caminho);
+        BufferedReader lerArquivo = new BufferedReader(arquivo);
+        return lerArquivo;
+    }
+}
---
+++ src/riodofogo/RioDoFogo.java
@@ -0,0 +1,16 @@
+package riodofogo;
+
+import riodofogo.telas.Principal;
+
+/**
+ *
+ * @author joao.franca
+ */
+public class RioDoFogo {
+    public static void main(String[] args) {
+        Principal p = new Principal();
+        p.setVisible(true);;
+        p.setLocationRelativeTo(null);
+        p.setTitle("Sistema de Ponto Eletrôncio Rio do Fogo");
+    }    
+}
