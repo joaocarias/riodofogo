5b9672eb0be44567021ea74583790a55d32a3f3f
João Carias <joaocariasdefranca@gmail.com>
2017-11-28 11:53:33 -0300

    implementação da classe ConexaoMySQL.java

--- src/Conexao/ConexaoMySQL.java
+++ src/Conexao/ConexaoMySQL.java
@@ -6,6 +6,8 @@
 package Conexao;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import riodofogo.Propriedades;
 
 /**
@@ -13,15 +15,28 @@ import riodofogo.Propriedades;
  * @author joao.franca
  */
 public class ConexaoMySQL implements Conexao {
-    private String status;  
+    private boolean status;  
     private String servidor;
     private String nomeBanco;
     private String url;
     private String usuario;
     private String senha;  
     private String driverName; 
-    private String porta;
+    private int porta;
     private Connection conn;
+    
+    public ConexaoMySQL(){
+        Propriedades p = new Propriedades();
+        
+        this.servidor = p.lerPropriedade("servidor_mysql");    //caminho do servidor do BD
+        this.nomeBanco = p.lerPropriedade("banco_mysql");        //nome do seu banco de dados
+        //é necessário usar ?zeroDateTimeBehavior=convertToNull para evitar o erro "java.sql.SQLException: Value '0000-00-00 00:00:00' can not be represented as java.sql.Date"
+        this.url = "jdbc:mysql://" + this.servidor + "/" + this.nomeBanco+"?zeroDateTimeBehavior=convertToNull";
+        this.usuario = p.lerPropriedade("usuario_mysql");        //nome de um usuário de seu BD      
+        this.senha = p.lerPropriedade("senha_mysql");      //sua senha de acesso    
+        this.driverName = "com.mysql.jdbc.Driver";          
+        this.porta = Integer.parseInt(p.lerPropriedade("porta_mysql"));
+    }
     
     @Override
     public boolean testarConexao() {
@@ -38,83 +53,120 @@ public class ConexaoMySQL implements Conexao {
 
     @Override
     public Connection criarCriacao() {
-        Propriedades p = new Propriedades();
-        return null;
+        this.conn = null;
+        
+        try {
+           Class.forName(this.getDriverName()).newInstance();
+           this.conn = DriverManager.getConnection(this.getUrl(), this.getUsuario(), this.getSenha());          
+            
+            //Testa sua conexão//  
+            if (this.conn != null){
+                this.setStatus(true);
+                return conn;
+            } else {
+                this.setStatus(false);
+                return null;
+            }            
+        } catch (ClassNotFoundException e) {  //Driver não encontrado
+            System.out.println("O driver expecificado nao foi encontrado.");            
+            System.err.println(e.getMessage());
+            return null;
+        } catch (SQLException e) {
+            //Não conseguindo se conectar ao banco
+            System.err.println(e.getMessage());
+            return null;        
+        }catch( Exception e){
+            System.err.println(e.getMessage());
+            return null;
+        }
     }
 
     @Override
     public boolean disconectar() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+            this.conn.close();
+            return true;
+        }catch(Exception e){ 
+            System.err.println(e.getMessage());
+            return false;
+        }
     }
 
     @Override
     public int getPorta() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.porta;
     }
 
     @Override
     public void setPorta(int porta) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.porta = porta;
     }
 
+    public String getDriverName() {
+        return driverName;
+    }
+
+    public void setDriverName(String driverName) {
+        this.driverName = driverName;
+    }
+    
     @Override
     public boolean getStatus() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.status;
     }
 
     @Override
     public void setStatus(boolean status) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.status = status;
     }
 
     @Override
     public String getServidor() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.servidor;
     }
 
     @Override
     public void setServidor(String servidor) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.servidor = servidor;
     }
 
     @Override
     public String getNomeBanco() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.nomeBanco;
     }
 
     @Override
     public void setNomeBanco(String nomeBanco) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.nomeBanco = nomeBanco;
     }
 
     @Override
     public String getUrl() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.url;
     }
 
     @Override
     public void setUrl(String url) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.url = url;
     }
 
     @Override
     public String getUsuario() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.usuario;
     }
 
     @Override
     public void setUsuario(String usuario) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.usuario = usuario;
     }
 
     @Override
     public String getSenha() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.senha;
     }
 
     @Override
     public void setSenha(String senha) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
+        this.senha = senha;
+    }    
 }
